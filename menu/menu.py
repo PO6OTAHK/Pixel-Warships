bd81c18fa9fac4e96544a0d22a612ccf2f351ec5
PO6OTAHK <koranov_2004@mail.ru>
2021-03-17 17:36:54 +0800

    Ерунда Данила 17.03))))))))))))))))))

--- menu/menu.py
+++ menu/menu.py
@@ -1,25 +1,40 @@
-import pygame
-import pygame_menu
-from pygame_menu import sound
-
-pygame.init()
-WIDTH = 704
-HEIGHT = 640
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
-engine = sound.Sound()
-engine.set_sound(sound.SOUND_TYPE_CLICK_MOUSE, 'menu_click.mp3')
-engine.set_sound(sound.SOUND_TYPE_OPEN_MENU, 'menu.mp3')
-
-mytheme = pygame_menu.themes.Theme(
-    image_path='D:\Projects\.vscode\Pixel Warships\kon1.jpg',
-    drawing_mode=pygame_menu.baseimage.IMAGE_MODE_REPEAT_XY)
-
+def createMenu(game_instance, screen):
+    engine = game_instance.pygame_menu.sound.Sound()
+    engine.set_sound(game_instance.pygame_menu.sound.SOUND_TYPE_CLICK_MOUSE, 'menu/menu_click.ogg')
+    background_image = game_instance.pygame_menu.baseimage.BaseImage(
+            image_path='image/kon1.jpg',
+            drawing_mode=game_instance.pygame_menu.baseimage.IMAGE_MODE_REPEAT_XY,
+            drawing_offset=(0,0)
+    )
+    mytheme = game_instance.pygame_menu.themes.Theme(
+            background_color=background_image,
+            title_background_color=(255, 255, 255),
+            title_font_color=(0, 0, 0),
+            widget_font=game_instance.FONT,
+            widget_background_color=(255, 255, 255),
+            title_font=game_instance.FONT,
+            widget_font_size=25,
+            title_font_size=35,
+            widget_font_color=(0, 0, 0),
+            selection_color=(0, 0, 0),
+            widget_margin=(0, 20),
+            widget_padding=10,
+            title_bar_style=game_instance.pygame_menu.widgets.MENUBAR_STYLE_SIMPLE
+    )
+    menu = game_instance.pygame_menu.Menu(
+        game_instance.HEIGHT_SCREEN, 
+        game_instance.WIDTH_SCREEN, 
+        'Hello',
+        theme=mytheme
+    )
+    menu.set_sound(engine, recursive=True)
+    menu.add_button('Play', game_instance.startGame) # В None функция игры или любая др.
+    menu.add_button('Options', None)
+    menu.add_button('Quit', game_instance.pygame_menu.events.EXIT)
     
-menu = pygame_menu.Menu(640, 704, mytheme, title='Здраствуйте')
+    menu.mainloop(screen)
 
-button = menu.add_button('Play', None) # В None функция игры или любая др.
-button1 = menu.add_button('Options', None)
-button2 = menu.add_button('Quit', None)
 
-menu.mainloop(screen)
+def start(game_instance):
+    createMenu(game_instance, game_instance.getScreen())
+    
--- ship/ship.py
+++ ship/ship.py
@@ -1,25 +1,46 @@
+import pygame
+def init(game_instance_local):
+    global game_instance
+    game_instance = game_instance_local
+
 class Ship(pygame.sprite.Sprite):
     """Класс описывающий корабль в который подаются: позиция, здоровье, текстуры, жив ли он, скорость, туррели, ширина, длина."""
-    def __init__(self, x, y, HP, ID_img, He_alive, speed, objectt, ssize, *groups):
-        super().__init__(*groups)
-        self.x = x
-        self.y = y
+    def __init__(self, x, y, *groups, HP=100, speed=5, objectt=None, size=(128, 64)):
+        super().__init__(groups)
+        self.ship_image = {
+                    'right': game_instance.pygame.image.load('ship/Textur/ShipV01right.png'), 
+                    'left': game_instance.pygame.image.load('ship/Textur/ShipV01left.png'),
+                    'up': game_instance.pygame.image.load('ship/Textur/ShipV01up.png'),
+                    'down': game_instance.pygame.image.load('ship/Textur/ShipV01down.png'),
+        }
         self.HP = HP
-        self.ID_img = ID_img
-        self.He_alive = He_alive
+        self.image = game_instance.pygame.transform.scale(self.ship_image['right'], (64, 34))
+        self.is_alive = True
         self.speed = speed
         self.objectt = objectt
-        self.ssize = ssize
+        self.size = size
+        self.x, self.y = x, y
+        self.rect = self.image.get_rect().move(64 * self.x, 34 * self.y)
     
-    def hit(self, HP, ):
+    def changeState(self, state):
+        if state == 'up' or state == 'down':
+            self.image = game_instance.pygame.transform.scale(self.ship_image[state], (34, 64))
+        else:
+            self.image = game_instance.pygame.transform.scale(self.ship_image[state], (64, 34))
+
+    def hit(self, damage):
         pass
 
-    def sink(self, HP):
-        if HP < 0:
+    def sink(self):
+        if self.HP < 0:
             return True
         else:
             return False
     
-    def plase_turret():
-       yes = ssize // tsize
+    def plase_turret(self):
+       yes = self.ssize // self.tsize
        return yes
+
+
+      
+        
